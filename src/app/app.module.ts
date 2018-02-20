5d306083c31e73882e4e0832303f26a658afc3a2
neo091 <mneo666@gmail.com>
2018-02-20 16:33:25 -0300

    header, footer, portafolios, about, producto listos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafoliosComponent } from './components/portafolios/portafolios.component';
 import { AboutComponent } from './components/about/about.component';
+import { ProductoComponent } from './components/producto/producto.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { AboutComponent } from './components/about/about.component';
     HeaderComponent,
     FooterComponent,
     PortafoliosComponent,
-    AboutComponent
+    AboutComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule
