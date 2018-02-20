375e1f67d628182d29aaa07a824b795923c48490
neo091 <mneo666@gmail.com>
2018-02-20 16:00:48 -0300

    header, footer, portafolios listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortafoliosComponent } from './components/portafolios/portafolios.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafoliosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/componetns/portafolios/portafolios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portafolios',
+  templateUrl: './portafolios.component.html',
+  styles: []
+})
+export class PortafoliosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
