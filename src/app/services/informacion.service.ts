dbc922d14bfa626e5ab70f18df34ae3f9a39cf52
neo091 <mneo666@gmail.com>
2018-02-21 11:08:29 -0300

    editando y agregando sobre nosotros en servicios de informacion

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -8,16 +8,37 @@ import { Http} from "@angular/http";
 @Injectable()
 export class InformacionService {
   info:any = {};
+  equipo:any[] = [];
   carga:boolean = false;
+  carga_sobre_nosotros_is:boolean = false;
 
 
   constructor(public http:Http) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info(){
+
     this.http.get("assets/data/info.paginas.json")
     .subscribe(data=>{
         //console.log(data.json());
         this.carga = true;
         this.info = data.json();
       })
+
   }
+
+  public carga_sobre_nosotros(){
+    this.http.get("https://appportafolios.firebaseio.com/equipo.json")
+    .subscribe(data=>{
+        console.log(data.json());
+        this.carga_sobre_nosotros_is = true;
+        this.equipo = data.json();
+      })
+  }
+
+
+
 
 }
