52a5a5eb2ca3791314b0a2c3bb2639402575d51a
neo091 <mneo666@gmail.com>
2018-02-21 10:13:06 -0300

    agregado informacion de servicios json

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Http} from "@angular/http";
+
+
+
+
 
 @Injectable()
 export class InformacionService {
+  info:any = {};
+  carga:boolean = false;
 
-  constructor() { }
+
+  constructor(public http:Http) {
+    this.http.get("assets/data/info.paginas.json")
+    .subscribe(data=>{
+        //console.log(data.json());
+        this.carga = true;
+        this.info = data.json();
+      })
+  }
 
 }
